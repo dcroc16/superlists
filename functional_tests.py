394b22d0a184360c4c791db65a5ada1bdf93429e
dcroc16 <davidwcrocker@gmail.com>
2015-02-07 14:10:11 -0600

    udated the FT to use unittest and added a bit of user story

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setup(self):
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_creating_a_new_list(self):
+		self.setup()
+		# User Stories
+		# Edith heard about a cool new online todo app, She goes
+		# to check out its homepage
+
+		self.browser.get('localhost:8000')
+
+		# She notices the page title and header mention to-do lists
+		self.assertEqual('To-Do',self.browser.title)
+		
+		# She is invited to enter a to-do item straight away
+		self.tearDown()
+		# She types "Buy Peacock Feathers" into a text box (Edith's Hoby)
+
+
+if __name__ == '__main__':
+	unittest.main()
