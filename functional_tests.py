c71f7a27193a0d1e1459ddfce88d5bd06e4081a7
dcroc16 <davidwcrocker@gmail.com>
2015-02-07 21:01:30 -0600

    added a template of html

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,12 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 	def setup(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-		
+
 	def tearDown(self):
 		self.browser.quit()
 
@@ -20,10 +21,27 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertEqual('To-Do lists', self.browser.title)
+		header_text = self.browser.find_element_by_id('h1').text
+		self.assertIn('To-Do',header_text)
 
+
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 		# She is invited to enter a to-do item straight away
 		
+
 		# She types "Buy Peacock Feathers" into a text box (Edith's Hoby)
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_id('tr')
+		self.assertTrue(
+				any(row.text == '1: Buy peacock feathers' for row in rows)
+			)
 		self.fail('Finish the test!')
 
 		# There is still a text box inviting her to add another item
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(res):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
-
+	return render(res, 'home.html')
